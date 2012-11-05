e19e9319d0e5b9d5469a3907e202a121dbef4881
Jose Ignacio Gil Jaldo <jjaldo@jjaldo-smeet.(none)>
2012-11-05 17:06:16 +0100

    Adding forgotten files

--- app/controllers/Pets.scala
+++ app/controllers/Pets.scala
@@ -3,14 +3,15 @@ package controllers
 import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
+import services.Pet
 
 object Pets extends Controller {
 
   val petSearch = Form(
     tuple(
       "typeId" -> optional(number),
-      "minPrice" -> number(0),
-      "maxPrice" -> optional(number(0))
+      "minPrice" -> longNumber,
+      "maxPrice" -> optional(longNumber)
     ) verifying("Invalid price range", result => result match {
       case (typeId, minPrice, None) => true
       case (typeId, minPrice, Some(maxPrice)) => maxPrice >= minPrice
@@ -26,13 +27,13 @@ object Pets extends Controller {
   def update(id : Long) = TODO
 
   def search = Action { implicit request =>
-    Ok(views.html.search(petSearch.fill((None, 0, None)), Nil))
+    Ok(views.html.search(petSearch.fill((None, 0L, None)), Nil, Pet.findPets((None, 0, None))))
   }
 
   def performSearch = Action { implicit request =>
     petSearch.bindFromRequest().fold(
-      hasErrors => Ok(views.html.search(petSearch, hasErrors.globalErrors))
-      , success => Ok(views.html.search(petSearch, Nil)))
+      hasErrors => Ok(views.html.search(petSearch, hasErrors.globalErrors, Set[Pet]()))
+      , success => Ok(views.html.search(petSearch, Nil, Pet.findPets(petSearch.get))))
   }
 
 }
--- app/services/Pet.scala
+++ app/services/Pet.scala
@@ -1,8 +1,42 @@
 package services
 
+import java.util.{Date, Calendar}
+import play.api.Play.current
+import anorm._
+import play.api.db.DB
+
 /**
  * @author: jjaldo
  */
-class Pet {
+class Pet(var id: Int, var name : String, var price : Long, var birthday : Calendar, var petTypeId : Int) {
 
+}
+
+object Pet {
+  def findPets(searchParams : (Option[Int], Long, Option[Long])) : Set[Pet] = {
+    val sql = "SELECT id, name, price, birthday, pet_type_id FROM pets WHERE "
+    val query = (searchParams match {
+      case (None, minPrice, Some(maxPrice)) => { SQL(sql + "price BETWEEN {minPrice} AND {maxPrice}")
+                                                .on("maxPrice" -> maxPrice,
+                                                    "minPrice" -> minPrice)
+                                               }
+      case (None, minPrice, None) => { SQL(sql + "price >= {minPrice}")
+                                          .on("minPrice" -> minPrice)
+                                     }
+      case (Some(typeId), minPrice, None) => { SQL(sql + "type_id = {typeId} AND price >= {minPrice}")
+                                                .on("typeId" -> typeId,
+                                                "minPrice" -> minPrice)
+                                             }
+      case (Some(typeId), minPrice, Some(maxPrice)) => { SQL(sql + "type_id = {typeId} AND price BETWEEN {minPrice} AND {maxPrice}")
+        .on("minPrice" -> minPrice)
+      }
+    })
+    DB.withConnection({ implicit c =>
+      query().map(row => {
+          val calendar = Calendar.getInstance()
+          calendar.setTime(row[Date]("birthday"))
+          new Pet(row[Int]("id"), row[String]("name"), row[Long]("price"), calendar, row[Int]("pet_type_id"))
+        }).toSet
+    })
+  }
 }
